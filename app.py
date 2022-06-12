df1b58494a11d312c6e99bf50b384aeb4065a0ba
namratawadekar42 <101054924+namratawadekar42@users.noreply.github.com>
2022-06-12 18:23:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import joblib
+model=joblib.load('spam-ham')
+st.title('SPAM-HAM CLASSIFIER')
+ip=st.text_input('enter your message')
+op=model.predict([ip])
+if st.botton('Predict'):
+  st.title(Op[0])
